0a008384ebef11b955b134fa6848eb9da4402640
Leaptail <52641374+Leaptail@users.noreply.github.com>
2023-10-01 21:25:59 +0800

    unable to recognise zombies accurately

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import pydirectinput
 import cv2
 
+#screen dimensions 2560 by 1440, midpoints 1281,721
 #Screenshots and so called Memory program
 num = 0
 #while True:
@@ -14,14 +15,32 @@ num = 0
     print("working")"""
 
 #lets start by making it run away from zombies
-"""for abc in range(1,6):
-    Tradefrom = pyautogui.locateCenterOnScreen("BuiltMem\Mem%d.png" % abc, confidence=0.55)
-    if Tradefrom == None:
-        print("notfound")
-        continue
-    else:
-        x, y = Tradefrom
-        print("found")"""
-for abc in range(1,10):
-    x,y =pyautogui.size()
-    print(x,y)
+while True:
+    for abc in range(1,26):
+        Tradefrom = pyautogui.locateCenterOnScreen("BuiltMem\Mem%d.png" % abc, grayscale = True, confidence=0.6)
+        if Tradefrom == None:
+            print("notfound")
+            pydirectinput.keyUp('d')
+            pydirectinput.keyUp('a')
+            pydirectinput.keyUp('s')
+            pydirectinput.keyUp('w')
+            continue
+        else:
+            x, y = Tradefrom
+            print("found")
+            if x-1281>=0:
+                pydirectinput.keyDown('a')
+                pydirectinput.keyUp('d')
+            elif x-1281<0:
+                pydirectinput.keyDown('d')
+                pydirectinput.keyUp('a')
+            if y-721>=0:
+                pydirectinput.keyDown('w')
+                pydirectinput.keyUp('s')
+            elif y-721<0:
+                pydirectinput.keyDown('s')
+                pydirectinput.keyUp('w')
+    
+
+
+
